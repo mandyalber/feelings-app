94e74958e47f44ba6e214947f67b06bf5764f338
Mandy Alber <mandy.alber@gmail.com>
2021-03-25 09:52:39 -0700

    set up store, add core to page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import store from './store';
 
 function App() {
-  return (
+  const [state, setState] = useState(store);
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Feelings Accordion
       </header>
+      <main>
+        <ul className="core-feelings">
+          {state.feelings1.map(feeling => <li>{feeling.name}</li>)}
+        </ul>
+
+      </main>
     </div>
   );
 }
